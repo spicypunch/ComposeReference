9290154ee10ec9b893fadf82419fc105f96d0ab2
spicypunch <miniguni66@naver.com>
2023-07-30 00:55:41 +0900

    ComposeReference

--- ComposeState/app/src/main/java/toss/next/composestate/MainActivity.kt
+++ ComposeState/app/src/main/java/toss/next/composestate/MainActivity.kt
@@ -3,44 +3,73 @@ package toss.next.composestate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import toss.next.composestate.ui.theme.ComposeStateTheme
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeStateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            HoseScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HoseScreen(viewModel: MainViewModel = viewModel()) {
+    val text1: MutableState<String> = remember {
+        mutableStateOf("Hello World")
+    }
+    var text2 by remember {
+        mutableStateOf("Hello World")
+    }
+    val (text3, setText) = remember {
+        mutableStateOf("Hello World")
+    }
+
+    val text4: State<String> = viewModel.liveData.observeAsState("Hello World")
+
+    Column {
+        /**
+         * 결국 데이터를 변경하려는 행위
+         * 이 행위를 하면 화면은 다시 그려진다.
+         */
+        Text(text = "Hello World")
+        Button(onClick = {}) {
+            text1.value = "변경"
+            text2 = "변경"
+//            text3 = "변경"  불가
+            setText("변경")
+            viewModel.changeValue("변경")
+        }
+        TextField(value = text3, onValueChange = setText)
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeStateTheme {
-        Greeting("Android")
+class MainViewModel : ViewModel() {
+    private val _value = mutableStateOf("Hello World")
+    val value: State<String> = _value
+
+    private val _liveData = MutableLiveData<String>()
+    val liveData: LiveData<String> = _liveData
+    fun changeValue(value: String) {
+        _value.value = value
+        _liveData.value = value
     }
 }
